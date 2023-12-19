720e5a9fc137d61d8b25ad2f4ee2564f01cc063c
Kabuto-Hido <ngobuituananh@gmail.com>
2023-12-19 15:14:54 +0700

    init

---
+++ src/main/java/com/bt1/qltv1/Qltv1Application.java
@@ -0,0 +1,17 @@
+package com.bt1.qltv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class Qltv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qltv1Application.class, args);
+
+    }
+
+}
