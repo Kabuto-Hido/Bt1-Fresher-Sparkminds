2154b3da3a12e5e4c43d74d201691f666b93c82b
Kabuto-Hido <ngobuituananh@gmail.com>
2023-12-21 18:25:48 +0700

    login with MFA (not done)

---
+++ src/main/java/com/bt1/qltv1/util/TwoFactorAuthenticationProvider.java
@@ -0,0 +1,6 @@
+package com.bt1.qltv1.util;
+
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+public class TwoFactorAuthenticationProvider extends DaoAuthenticationProvider {
+}
